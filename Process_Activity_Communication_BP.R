5af31ca32010d15ffe8905085b2ad9b6967c0509
brainfood <nif@mail.med.upenn.edu>
2017-05-24 15:58:48 -0400

    Add circadian stats for lux, mobility, and mobility radius to processing script.

--- Process_Activity_Communication_BP.R
+++ Process_Activity_Communication_BP.R
@@ -33,6 +33,15 @@ bloodpressure.HR <- na.omit(bloodpressure.HR)
 # Energy expenditure data
 energy <- read.csv("chronobiome/Carsten_GingerIO/Activity_data/HCR001-009_ActigraphyScoring_kcals&METrate.csv")
 
+# Circadian stats for lux, mobility, and mobility radius
+lux.circ_stats = read.csv("chronobiome/Carsten_GingerIO/Activity_data/Lux_circadian_stats_over_4_months_1min-res.csv")
+mobility.circ_stats = read.csv("chronobiome/Carsten_GingerIO/Communication_data/Mobility_circadian_stats_over_4_months_1min-res.csv")
+mobility_radius.circ_stats = read.csv("chronobiome/Carsten_GingerIO/Communication_data/MobilityRadius_circadian_stats_over_4_months_1min-res.csv")
+
+# Eliminate rows with missing values from the mobility and mobility radius data
+mobility.circ_stats.NoA = na.omit(mobility.circ_stats)
+mobility_radius.circ_stats.NoA = na.omit(mobility_radius.circ_stats)
+
 
 #######################################################
 # Process data, combine activity and communication sets
@@ -42,9 +51,23 @@ communication.NoNA$Date <- substr(communication.NoNA$start, 0, 10)
 communication.NoNA$Days <- chron(as.character(communication.NoNA$Date), 
                                  format=c(dates="y-m-d")) - chron("2014-10-21", 
                                                                   format=c(dates="y-m-d"))
+mobility.circ_stats.NoA$Date <- substr(mobility.circ_stats.NoA$Start.Time, 0, 10)
+mobility.circ_stats.NoA$Days <- chron(as.character(mobility.circ_stats.NoA$Date), 
+                                      format=c(dates="y-m-d")) - chron("2014-10-21", 
+                                                                       format=c(dates="y-m-d"))
+mobility_radius.circ_stats.NoA$Date <- substr(mobility_radius.circ_stats.NoA$Start.Time, 0, 10)
+mobility_radius.circ_stats.NoA$Days <- chron(as.character(mobility_radius.circ_stats.NoA$Date), 
+                                             format=c(dates="y-m-d")) - chron("2014-10-21", 
+                                                                              format=c(dates="y-m-d"))
+
 activity$Days <- chron(as.character(activity$Date), 
                        format=c(dates="m/d/y")) - chron("2014-10-21", 
                                                         format=c(dates="y-m-d"))
+lux.circ_stats$Date <- substr(lux.circ_stats$Start.Time, 0, 10)
+lux.circ_stats$Days <- chron(as.character(lux.circ_stats$Date), 
+                             format=c(dates="y-m-d")) - chron("2014-10-21", 
+                                                              format=c(dates="y-m-d"))
+
 bloodpressure.HR$Date <- substr(bloodpressure.HR$Start.Time, 0, 10)
 bloodpressure.HR$Days <- chron(as.character(bloodpressure.HR$Date), 
                                format=c(dates="y-m-d")) - chron("2014-10-21", 
@@ -59,9 +82,28 @@ communication.NoNA$TimeSubjectIndex <-
   paste(communication.NoNA$TimeIndex,
         communication.NoNA$user_id,
         sep = "_")
+mobility.circ_stats.NoA$Hour <- as.numeric(substr(mobility.circ_stats.NoA$Start.Time, 12, 13))
+mobility.circ_stats.NoA$TimeIndex <-
+    mobility.circ_stats.NoA$Days * 24 + mobility.circ_stats.NoA$Hour
+mobility.circ_stats.NoA$TimeSubjectIndex <-
+    paste(mobility.circ_stats.NoA$TimeIndex,
+          mobility.circ_stats.NoA$user_id,
+          sep = "_")
+mobility_radius.circ_stats.NoA$Hour <- as.numeric(substr(mobility_radius.circ_stats.NoA$Start.Time, 12, 13))
+mobility_radius.circ_stats.NoA$TimeIndex <-
+    mobility_radius.circ_stats.NoA$Days * 24 + mobility_radius.circ_stats.NoA$Hour
+mobility_radius.circ_stats.NoA$TimeSubjectIndex <-
+    paste(mobility_radius.circ_stats.NoA$TimeIndex,
+          mobility_radius.circ_stats.NoA$user_id,
+          sep = "_")
+
 activity$TimeIndex <- activity$Days * 24 + activity$Hour
 activity$TimeSubjectIndex <-
   paste(activity$TimeIndex, activity$user_id, sep = "_")
+lux.circ_stats$Hour <- as.numeric(substr(lux.circ_stats$Start.Time, 12, 13))
+lux.circ_stats$TimeIndex <- lux.circ_stats$Days * 24 + lux.circ_stats$Hour
+lux.circ_stats$TimeSubjectIndex <-
+    paste(lux.circ_stats$TimeIndex, lux.circ_stats$user_id, sep = "_")
 
 bloodpressure.HR$TimeIndex <- bloodpressure.HR$Days * 24 + bloodpressure.HR$Hour
 bloodpressure.HR$TimeSubjectIndex <-
@@ -128,7 +170,10 @@ write.csv(Heartrate.BP, "heartrate.bp.csv")
 
 communication.df <- communication.NoNA %>% 
   dplyr::filter(TimeSubjectIndex %in%
-                  intersect(activity$TimeSubjectIndex, communication.NoNA$TimeSubjectIndex)) %>%
+                  intersect(activity$TimeSubjectIndex, communication.NoNA$TimeSubjectIndex),
+                #Lux data contain slightly fewer data points than the activity data
+                TimeSubjectIndex %in%
+                    intersect(activity$TimeSubjectIndex, lux.circ_stats$TimeSubjectIndex)) %>%
   dplyr::group_by(TimeSubjectIndex) %>%
   dplyr::summarise(Mobility = mean(Mobility),
                    Mobility.Radius = mean(Mobility.Radius),
@@ -141,13 +186,38 @@ communication.df <- communication.NoNA %>%
                    Communication.amplitude = mean(instantaneous.amplitude),
                    Communication.period = mean(instantaneous.period),
                    Communication.phase = mean(instantaneous.phase))
+mobility.df <- mobility.circ_stats.NoA %>% 
+    dplyr::filter(TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, communication.NoNA$TimeSubjectIndex),
+                  #Lux data contain slightly fewer data points than the activity data
+                  TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, lux.circ_stats$TimeSubjectIndex)) %>%
+    dplyr::group_by(TimeSubjectIndex) %>%
+    dplyr::summarise(circadian.signal.mob = mean(circadian.signal),
+                     Mobility.amplitude = mean(instantaneous.amplitude),
+                     Mobility.period = mean(instantaneous.period),
+                     Mobility.phase = mean(instantaneous.phase))
+mobilityRadius.df <- mobility_radius.circ_stats.NoA %>% 
+    dplyr::filter(TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, communication.NoNA$TimeSubjectIndex),
+                  #Lux data contain slightly fewer data points than the activity data
+                  TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, lux.circ_stats$TimeSubjectIndex)) %>%
+    dplyr::group_by(TimeSubjectIndex) %>%
+    dplyr::summarise(circadian.signal.mobR = mean(circadian.signal),
+                     MobilityRadius.amplitude = mean(instantaneous.amplitude),
+                     MobilityRadius.period = mean(instantaneous.period),
+                     MobilityRadius.phase = mean(instantaneous.phase))
 
 # Subset activity dataset to include only TimeSubjectIndex recordings
 # also present in the communication dataset, take average of each variable at 
 # each Subject/Hour combination
 activity.df <- activity %>% 
  dplyr::filter(TimeSubjectIndex %in%
-               intersect(activity$TimeSubjectIndex,communication.NoNA$TimeSubjectIndex)) %>%
+                   intersect(activity$TimeSubjectIndex,communication.NoNA$TimeSubjectIndex),
+               #Lux data contain slightly fewer data points than the activity data
+               TimeSubjectIndex %in%
+                   intersect(activity$TimeSubjectIndex, lux.circ_stats$TimeSubjectIndex)) %>%
   dplyr::group_by(TimeSubjectIndex) %>%
   dplyr::summarise(Steps = mean(Steps), Axis1 = mean(Axis1),
                    Axis2 = mean(Axis2), Axis3 = mean(Axis3),
@@ -157,13 +227,27 @@ activity.df <- activity %>%
                    activity.amplitude=mean(instantaneous.amplitude),
                    activity.period = mean(instantaneous.period),
                    activity.phase = mean(instantaneous.phase))
+lux.df <- lux.circ_stats %>% 
+    dplyr::filter(TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, communication.NoNA$TimeSubjectIndex),
+                  #Lux data contain slightly fewer data points than the activity data
+                  TimeSubjectIndex %in%
+                      intersect(activity$TimeSubjectIndex, lux.circ_stats$TimeSubjectIndex)) %>%
+    dplyr::group_by(TimeSubjectIndex) %>%
+    dplyr::summarise(circadian.signal.lux = mean(circadian.signal),
+                     Lux.amplitude = mean(instantaneous.amplitude),
+                     Lux.period = mean(instantaneous.period),
+                     Lux.phase = mean(instantaneous.phase))
 
-communication.activity <- dplyr::full_join(activity.df, communication.df, by="TimeSubjectIndex")
+# communication.activity <- dplyr::full_join(activity.df, communication.df, by="TimeSubjectIndex")
+# 
+# rownames(communication.activity) <- 1:nrow(communication.activity)
+# communication.activity <- data.frame(communication.activity)
 
-rownames(communication.activity) <- 1:nrow(communication.activity)
-communication.activity <- data.frame(communication.activity)
-
-communication.activity <- dplyr::full_join(activity.df, communication.df, by="TimeSubjectIndex")
+communication.activity <- dplyr::full_join(activity.df, communication.df, by="TimeSubjectIndex") %>% 
+    dplyr::full_join(mobility.df, by="TimeSubjectIndex") %>% 
+    dplyr::full_join(mobilityRadius.df, by="TimeSubjectIndex") %>% 
+    dplyr::full_join(lux.df, by="TimeSubjectIndex")
 
 
 # Transform variables in communication.activity to improve normality
@@ -182,6 +266,10 @@ transformed.communication.activity <- communication.activity %>%
                    activity.amplitude = activity.amplitude,
                    activity.period = activity.period,
                    activity.phase = activity.phase,
+                   circadian.signal.lux = circadian.signal.lux,
+                   Lux.amplitude = Lux.amplitude,
+                   Lux.period = Lux.period,
+                   Lux.phase = Lux.phase,
                    log.Mobility = log(Mobility + 1),
                    log.Mobility.Radius = log(Mobility.Radius + 1),
                    sqrt.Interaction.Diversity = sqrt(Interaction.Diversity),
@@ -192,7 +280,15 @@ transformed.communication.activity <- communication.activity %>%
                    circadian.signal.com = circadian.signal.com,
                    Communication.amplitude = Communication.amplitude,
                    Communication.period = Communication.period,
-                   Communication.phase = Communication.phase)
+                   Communication.phase = Communication.phase,
+                   circadian.signal.mob = circadian.signal.mob,
+                   Mobility.amplitude = Mobility.amplitude,
+                   Mobility.period = Mobility.period,
+                   Mobility.phase = Mobility.phase,
+                   circadian.signal.mobR = circadian.signal.mobR,
+                   MobilityRadius.amplitude = MobilityRadius.amplitude,
+                   MobilityRadius.period = MobilityRadius.period,
+                   MobilityRadius.phase = MobilityRadius.phase)
 
 # energy
 energy.df <- energy %>%
